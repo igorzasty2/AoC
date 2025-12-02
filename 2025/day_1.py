6e69c0f9ee59996ea8720a1adbfbed5f76ca4467
Igor Osiakowski <igorzasty2@wp.pl>
2025-12-02 08:00:44 +0100

    day 1 solved better

--- 2025/day_1.py
+++ 2025/day_1.py
@@ -1,8 +1,4 @@
 data = open("data/day_1.txt").read().split("\n")[:-1]
-# licznik od 0 do 99, jeśli przekręcisz na 100 to wraca się do 0
-# part 1, ile razy kończy na zerze
-# part 2, ile razy przechodzi przez zero
-
 
 # Part 1
 safe_num = 50
@@ -15,12 +11,10 @@ for row in data:
         zeroes += 1
 print(f"Part 1: {zeroes}")
 
-
 # Part 2
 safe_num = 50
 free_pass = False
 zero_crosses = 0
-
 
 for row in data:
     dir = 1 if row[0] == "R" else -1
